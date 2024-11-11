488ea4794ff587e892eccab92836cce56bd03e07
Florin Georgescu <flo_georgescu@hotmail.com>
2024-11-11 21:39:21 +0200

    Hiding controls and logo via query params

--- src/app/view/ZoomBar.ts
+++ src/app/view/ZoomBar.ts
@@ -1,83 +1,86 @@
 import MyScale from "../../utils/MyScale";
-import {Application, Container} from "pixi.js";
-import {CircleButton} from "./ui/CircleButton";
+import { Application, Container } from "pixi.js";
+import { CircleButton } from "./ui/CircleButton";
 
 export class ZoomBar extends Container {
+  onChangeProc: Function;
 
-    onChangeProc: Function;
+  proc: number;
 
+  zoomIn: Function;
+  zoomOut: Function;
+  autoMove: Function;
+  disableMove: Function;
 
-    proc: number;
+  constructor(app: Application) {
+    super();
+    // Check URL parameters for controls visibility
+    const urlParams = new URLSearchParams(window.location.search);
+    const showControls = urlParams.get("controls") !== "false";
 
-    zoomIn: Function;
-    zoomOut: Function;
-    autoMove: Function;
-    disableMove: Function;
-
-    constructor(app: Application) {
-
-        super();
-        app.stage.addChild(this);
-        let autoplay = true;
-        let main = this;
-
-        MyScale.setup(this, {
-            scalePortrait: 1,
-            scaleLandscape: 0.5,
-            bottom: 600,
-            right: 120,
-            onRescale: () => {
-            }
-        });
-
-        this.onChangeProc = (proc: number) => {
-            if (this.proc != proc) {
-            }
-            this.proc = proc;
-        }
-
-
-        let zoomIn = new CircleButton(app);
-        this.addChild(zoomIn);
-
-        zoomIn.onClick = () => {
-            main.zoomIn();
-        }
-        zoomIn.addIcon('plus');
-
-        let zoomOut = new CircleButton(app);
-        this.addChild(zoomOut);
-        zoomOut.y = 150;
-        zoomOut.onClick = () => {
-            main.zoomOut();
-        }
-        zoomOut.addIcon('minus');
-
-        let pauseBTn = new CircleButton(app);
-        this.addChild(pauseBTn);
-        pauseBTn.addIcon('pause');
-        pauseBTn.y = 300;
-        pauseBTn.onClick = () => {
-            autoplay = false;
-            playBtn.visible = true;
-            pauseBTn.visible = false;
-            main.autoMove(autoplay);
-        }
-
-        let playBtn = new CircleButton(app);
-        this.addChild(playBtn);
-        playBtn.addIcon('play');
-        playBtn.y = 300;
-        playBtn.onClick = () => {
-            autoplay = true;
-            playBtn.visible = false;
-            pauseBTn.visible = true;
-            main.autoMove(autoplay);
-        }
-        playBtn.visible = false;
-
-        this.disableMove = () => {
-            autoplay = false;
-        }
+    if (!showControls) {
+      return; // Exit early if controls should be hidden
     }
+
+    app.stage.addChild(this);
+    let autoplay = true;
+    let main = this;
+
+    MyScale.setup(this, {
+      scalePortrait: 1,
+      scaleLandscape: 0.5,
+      bottom: 600,
+      right: 120,
+      onRescale: () => {},
+    });
+
+    this.onChangeProc = (proc: number) => {
+      if (this.proc != proc) {
+      }
+      this.proc = proc;
+    };
+
+    let zoomIn = new CircleButton(app);
+    this.addChild(zoomIn);
+
+    zoomIn.onClick = () => {
+      main.zoomIn();
+    };
+    zoomIn.addIcon("plus");
+
+    let zoomOut = new CircleButton(app);
+    this.addChild(zoomOut);
+    zoomOut.y = 150;
+    zoomOut.onClick = () => {
+      main.zoomOut();
+    };
+    zoomOut.addIcon("minus");
+
+    let pauseBTn = new CircleButton(app);
+    this.addChild(pauseBTn);
+    pauseBTn.addIcon("pause");
+    pauseBTn.y = 300;
+    pauseBTn.onClick = () => {
+      autoplay = false;
+      playBtn.visible = true;
+      pauseBTn.visible = false;
+      main.autoMove(autoplay);
+    };
+
+    let playBtn = new CircleButton(app);
+    this.addChild(playBtn);
+    playBtn.addIcon("play");
+    playBtn.y = 300;
+    playBtn.onClick = () => {
+      autoplay = true;
+      playBtn.visible = false;
+      pauseBTn.visible = true;
+      main.autoMove(autoplay);
+    };
+    playBtn.visible = false;
+
+    this.disableMove = () => {
+      autoplay = false;
+    };
+  }
 }
--- src/app/view/ui/Logo.ts
+++ src/app/view/ui/Logo.ts
@@ -5,25 +5,31 @@ import CustomTextures from "../../../utils/CustomTextures";
 export class Logo extends Container {
   constructor(app: Application) {
     super();
-    app.stage.addChild(this);
 
-    MyScale.setup(this, {
-      scalePortrait: 0.5,
-      scaleLandscape: 0.25,
-      top: 300,
-      left: 700,
-      onRescale: () => {},
-    });
+    const urlParams = new URLSearchParams(window.location.search);
+    const showLogo = urlParams.get("logo") !== "false";
 
-    const img = new Sprite(CustomTextures.textures.logo);
-    img.anchor.set(0.5);
-    img.scale.set(0.3);
-    img.cursor = 'pointer';
-    this.addChild(img);
+    if (showLogo) {
+      app.stage.addChild(this);
 
-    img.interactive = true;
-    img.on("pointerdown", () => {
-      window.open("https://www.taraxa.io/");
-    });
+      MyScale.setup(this, {
+        scalePortrait: 0.5,
+        scaleLandscape: 0.25,
+        top: 300,
+        left: 700,
+        onRescale: () => {},
+      });
+
+      const img = new Sprite(CustomTextures.textures.logo);
+      img.anchor.set(0.5);
+      img.scale.set(0.3);
+      img.cursor = "pointer";
+      this.addChild(img);
+
+      img.interactive = true;
+      img.on("pointerdown", () => {
+        window.open("https://www.taraxa.io/");
+      });
+    }
   }
 }
